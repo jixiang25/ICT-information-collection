1a11f5476fd911961bdb4b36031463fabd35c483
jixiang <jixiang@ict.ac.cn>
2019-10-06 10:20:37 +0800

    finish extrac_links

---
+++ extrac_links.py
@@ -0,0 +1,112 @@
+import requests
+import queue
+from time import sleep
+from bs4 import BeautifulSoup
+
+
+class NetSpider(object):
+    def __init__(self):
+        self.links = list()
+        self.links_mp = dict()
+        self.pdf_links = list()
+        self.pdf_mp = dict()
+        self.except_cnt = 0
+        self.ALLOW_URL_PREFIX = [
+            'http://www.ict.cas.cn',
+            'http://www.ict.ac.cn',
+        ]
+        self.FORBIDDEN_URL_SUFFIX = [
+            '.jpg',
+            '.png',
+            '.gif',
+        ]
+
+    def __match_pattern(self, text, pattern, type):
+        pattern_len = len(pattern)
+        text_len = len(text)
+        if pattern_len > text_len:
+            return False
+        # print(text[:pattern_len], pattern)
+        if type == 'prefix':
+            return text[:pattern_len] == pattern
+        else:
+            return text[text_len - pattern_len: text_len] == pattern
+
+    def __url_spread(self, url, Q):
+        r = requests.get(url)
+        r.encoding = 'utf-8'
+        soup = BeautifulSoup(r.text, features='lxml')
+        a_links = soup.find_all('a')
+        for a_link_label in a_links:
+            if a_link_label.get('href') is not None:
+                a_link = a_link_label['href']
+            else:
+                continue
+            a_link = ''.join(a_link.split())
+            # 前缀是./先做替换
+            if self.__match_pattern(a_link, './', type='prefix'):
+                if url[-1] == '/':
+                    a_link = url + a_link[2:]
+                else:
+                    a_link = url + '/' + a_link[2:]
+            # 出现就不再访问
+            if self.links_mp.get(a_link) is not None:
+                continue
+            if a_link == 'http://www.ict.cas.cn/yjdgs/200902/t20090204_2179171.html':
+                raise ValueError(url)
+            # 前缀检查
+            pass_prefix_check = False
+            for prefix in self.ALLOW_URL_PREFIX:
+                if self.__match_pattern(a_link, prefix, type='prefix'):
+                    pass_prefix_check = True
+                    break
+            if not pass_prefix_check:
+                continue
+            # 后缀检查
+            pass_suffix_check = True
+            for suffix in self.FORBIDDEN_URL_SUFFIX:
+                if self.__match_pattern(a_link, suffix, type='suffix'):
+                    pass_suffix_check = False
+                    break
+            if not pass_suffix_check:
+                continue
+            # 检查是不是pdf
+            if self.__match_pattern(a_link, 'pdf', type='suffix'):
+                if self.pdf_mp.get(a_link) is None:
+                    self.pdf_links.append(a_link)
+                continue
+            # 入队并标记
+            Q.put(a_link)
+            self.links.append(a_link)
+            self.links_mp[a_link] = 1
+
+    def BFS(self):
+        Q = queue.Queue()
+        index_url = 'http://www.ict.ac.cn'
+        Q.put(index_url)
+        self.links.append(index_url)
+        self.links_mp[index_url] = 1
+        cnt = 1
+        while not Q.empty():
+            url = Q.get()
+            print(url, cnt)
+            self.__url_spread(url, Q)
+            cnt += 1
+
+    def write_links_to_file(self, file_dir='./url.txt', pdf_file_dir='./pdf_url.txt'):
+        with open(file_dir, 'w') as f:
+            for a_link in self.links:
+                f.write(a_link + '\n')
+        with open(pdf_file_dir, 'w') as f:
+            for a_link in self.pdf_links:
+                f.write(a_link + '\n')
+
+def main():
+    spider = NetSpider()
+    spider.BFS()
+    spider.write_links_to_file()
+    print(spider.except_cnt)
+
+
+if __name__ == '__main__':
+    main()
